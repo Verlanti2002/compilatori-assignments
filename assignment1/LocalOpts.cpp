f8193e34bec402546352ab91af9127b0fdd9b8c0
Verlanti2002 <verlanti2002@gmail.com>
2025-03-20 19:14:16 +0100

    create environment for first assignment

---
+++ assignment1/LocalOpts.cpp
@@ -0,0 +1,68 @@
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Module.h"
+
+using namespace llvm;
+
+namespace {
+
+
+// New PM implementation
+struct LocalOptsPass: PassInfoMixin<LocalOptsPass> {
+  // Main entry point, takes IR unit to run the pass on (&F) and the
+  // corresponding pass manager (to be queried if need be)
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+
+  	bool Transformed = runOnFunction(F);
+  	return (Transformed ? PreservedAnalyses::none() : PreservedAnalyses::all());
+  }
+
+  bool runOnBasicBlock(BasicBlock &B) {
+    bool Transformed = false;
+    
+    return Transformed;
+  }
+
+  bool runOnFunction(Function &F) {
+    bool Transformed = false;
+    for (auto &BB : F) {
+      if (runOnBasicBlock(BB)) {
+        Transformed = true;
+      }
+    }
+    return Transformed;
+  }
+
+
+  // Without isRequired returning true, this pass will be skipped for functions
+  // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+  // all functions with optnone.
+  static bool isRequired() { return true; }
+};
+} // namespace
+
+
+//-----------------------------------------------------------------------------
+// New PM Registration
+//-----------------------------------------------------------------------------
+llvm::PassPluginLibraryInfo getTestPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "LocalOptsPass", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "local-opts") {
+                    FPM.addPass(LocalOptsPass());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getTestPassPluginInfo();
+}
---
+++ assignment1/test/Test.c
@@ -0,0 +1,5 @@
+int algebraic_identity(int x) {
+    int a = x + 0;  // Deve diventare solo 'x'
+    int b = x * 1;  // Deve diventare solo 'x'
+    return a + b;
+}
